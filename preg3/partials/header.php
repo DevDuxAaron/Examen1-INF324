b0e9f551253f9d6627edeade059ec0be68cd5bdc
Aaron Joel <48360535+DevDuxAaron@users.noreply.github.com>
2022-10-12 09:34:39 -0400

    Delete preg3 directory

--- preg3/partials/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    // if (!isset($_SESSION["id"])) {
-    //     header("Location: /preg3/login.php");
-    // }
-?>
-<header>
-    <a href="/loginphp">Mi sistema para INF-324</a>
-</header>
